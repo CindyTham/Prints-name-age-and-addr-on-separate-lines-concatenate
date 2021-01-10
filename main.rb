6090e40da10744a073a395be43f3e41799895ab9
Cindy Tham <cindytham@hotmail.com>
2021-01-10 19:40:42 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,11 @@
+name = 'Cindy'
+
+age = 21
+
+address = '88 Perfect Avenue'
+
+puts name
+puts age
+puts address
+
+puts "Your name is #{name}, your age is #{age} and you're address is #{address}!"
